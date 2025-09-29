8e4aa29d2cb03ef22dc3205a6ae0d9f26a422550
ibrahmenaser <i.naser4040@gmail.com>
2025-09-29 19:28:53 +0300

    fix multiply.test file

--- __tests__/multiply.test.ts
+++ __tests__/multiply.test.ts
@@ -2,7 +2,7 @@ import { multiply } from "../src/lib/multiply";
 
 describe("Testing Multiply Method", () => {
   test("Multiply Two Numbers (2, 3)", async () => {
-    expect(multiply(2, 3)).toEqual(3);
+    expect(multiply(2, 3)).toEqual(6);
 
     return;
   });
